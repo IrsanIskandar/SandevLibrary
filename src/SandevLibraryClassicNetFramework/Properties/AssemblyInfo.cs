0e510993c06cab3a620d07ad1f3becdc13a76ea1
AnoneIsme <Putra.Iskandar@outlook.com>
2025-08-29 20:55:13 +0700

    Update Library For NET8 & NET9; Update Library For Classic Net Framework 4.8;

--- src/SandevLibraryClassicNetFramework/Properties/AssemblyInfo.cs
+++ src/SandevLibraryClassicNetFramework/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SandevLibraryClassicNetFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Reusable library for .NET Framework 4.8 Security Algorithm, Enryption, Decryption, Dapper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Irsan.Dev")]
 [assembly: AssemblyProduct("SandevLibraryClassicNetFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Irsan.Dev")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.5.1.0")]
+[assembly: AssemblyFileVersion("2.5.1.0")]
