3785b4b021cc688353b53b7e57d8e09a65047a19
Mang Adom <irsan.putra@internationalsos.com>
2025-10-07 15:08:39 +0700

    Upgrade Version 2.5.1 to 2.5.2; Upgrade Library From System.Data.SqlClient to Microsoft.Data.SqlClient;

--- src/SandevLibrary/MicroORM/StoredProcedureExecutorSystemDataSqlClient.cs
+++ src/SandevLibrary/MicroORM/StoredProcedureExecutorSystemDataSqlClient.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
 using System;
 using System.Collections.Generic;
